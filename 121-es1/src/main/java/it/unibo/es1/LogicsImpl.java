65cf3ab3b235edd014abe2d564c388611b1082e6
Wanes01 <emir.wanes.aouioua@gmail.com>
2024-12-09 17:53:20 +0100

    121 completed

--- 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
+++ 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
@@ -1,46 +1,61 @@
 package it.unibo.es1;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LogicsImpl implements Logics {
 
+	private final List<Integer> values;
+
 	public LogicsImpl(int size) {
-		//TODO Auto-generated constructor stub
+		if (size < 1) {
+			throw new IllegalArgumentException("Invalid number of buttons");
+		}
+		this.values = new ArrayList<>(size);
+		for (int i = 0; i < size; i++) {
+			this.values.add(0);
+		}
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'size'");
+		return this.values.size();
 	}
 
 	@Override
 	public List<Integer> values() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'values'");
+		return new ArrayList<>(this.values);
 	}
 
 	@Override
 	public List<Boolean> enablings() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'enablings'");
+		return this.values.stream()
+				.map(val -> val < this.values.size())
+				.toList();
 	}
 
 	@Override
 	public int hit(int elem) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'hit'");
+		if (elem >= this.values.size()) {
+			throw new IllegalArgumentException("There is no button of index " + elem);
+		}
+		this.values.set(elem,
+				this.values.get(elem) + 1);
+		return this.values.get(elem);
 	}
 
 	@Override
 	public String result() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'result'");
+		return this.values.stream()
+				.map(val -> String.valueOf(val))
+				.collect(Collectors.joining("|", "<<", ">>"));
 	}
 
 	@Override
 	public boolean toQuit() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'toQuit'");
+		return this.values.stream()
+				.filter(val -> val == this.values.get(0))
+				.count() == this.values.size();
 	}
 }
